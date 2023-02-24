a21a32034135272f08f5a2f64598b1d0a86dd0d2
soorokim <rlatnfh99@gmail.com>
2023-02-25 05:14:06 +0900

    setting: styled-components 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,73 @@
 import { useState } from 'react';
+import styled from 'styled-components';
 import reactLogo from './assets/react.svg';
 import './App.css';
+
+const Wrapper = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+`;
+
+const Card = styled.div`
+  padding: 2em;
+`;
+
+const Logo = styled.img`
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+
+  :hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+`;
+
+const ReactLogo = styled(Logo)`
+  @keyframes logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  :hover {
+    filter: drop-shadow(0 0 2em #61dafbaa);
+  }
+  animation: logo-spin infinite 20s linear;
+`;
+
+const ReadTheDocs = styled.p`
+  color: #888;
+`;
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <Wrapper>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <Logo src="/vite.svg" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <ReactLogo src={reactLogo} alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <Card>
         <button onClick={() => setCount((_count) => _count + 1)} type="button">
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
+      </Card>
+      <ReadTheDocs>Click on the Vite and React logos to learn more</ReadTheDocs>
+    </Wrapper>
   );
 }
 
