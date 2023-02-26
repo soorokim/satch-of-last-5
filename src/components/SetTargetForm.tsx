4325e00ee635adac55fad66abcbe701b08b6c9f8
soorokim <rlatnfh99@gmail.com>
2023-02-26 19:06:42 +0900

    refactor: goal입력후 메인페이지로 이동

--- src/components/SetTargetForm.tsx
+++ src/components/SetTargetForm.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import useGoalList from '../hooks/useGoalList';
 
@@ -134,6 +135,7 @@ const SetTargetForm = () => {
   const [isValid, setIsValid] = useState(true);
   const today = new Date();
   const { createGoal } = useGoalList();
+  const navigate = useNavigate();
 
   const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
@@ -145,6 +147,7 @@ const SetTargetForm = () => {
         emoticon: '',
         price,
       });
+      navigate('/');
     }
   };
 
