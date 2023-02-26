73a7be6f687cfa57a69788b1cbb414a51743ddb0
soorokim <rlatnfh99@gmail.com>
2023-02-26 18:01:37 +0900

    refactor: hooks수정

--- src/hooks/useGoal.ts
+++ src/hooks/useGoal.ts
@@ -1,51 +1,59 @@
+import { useCallback } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { v4 as uuidv4 } from 'uuid';
 import { Goal, goalListState, Satch } from '../atoms/goalList';
 
-const useGoal = (goalId: Goal['id']) => {
+const useGoal = (goalId?: Goal['id']) => {
   const setGoalList = useSetRecoilState(goalListState);
-  const createSatch = (satch: Omit<Satch, 'id'>) => {
-    setGoalList((prev) => {
-      const copyData = JSON.parse(JSON.stringify(prev)) as Goal[];
-      const index = copyData.findIndex((goal) => goal.id === goalId);
+  const createSatch = useCallback(
+    (satch: Omit<Satch, 'id'>) => {
+      setGoalList((prev) => {
+        const copyData = JSON.parse(JSON.stringify(prev)) as Goal[];
+        const index = copyData.findIndex((goal) => goal.id === goalId);
 
-      copyData[index] = {
-        ...copyData[index],
-        satchList: [...copyData[index].satchList, { ...satch, id: uuidv4() }],
-      };
+        copyData[index] = {
+          ...copyData[index],
+          satchList: [...copyData[index].satchList, { ...satch, id: uuidv4() }],
+        };
 
-      return copyData;
-    });
-  };
+        return copyData;
+      });
+    },
+    [goalId],
+  );
 
-  const updateSatch = (satchId: Satch['id']) => (satch: Partial<Omit<Satch, 'id'>>) => {
-    setGoalList((prev) => {
-      const copyData = JSON.parse(JSON.stringify(prev)) as Goal[];
-      const index = copyData.findIndex((goal) => goal.id === goalId);
-      const satchIndex = copyData[index].satchList.findIndex((data) => data.id === satchId);
+  const updateSatch = useCallback(
+    (satchId: Satch['id']) => (satch: Partial<Omit<Satch, 'id'>>) => {
+      setGoalList((prev) => {
+        const copyData = JSON.parse(JSON.stringify(prev)) as Goal[];
+        const index = copyData.findIndex((goal) => goal.id === goalId);
+        const satchIndex = copyData[index].satchList.findIndex((data) => data.id === satchId);
 
-      copyData[index].satchList[satchIndex] = {
-        ...copyData[index].satchList[satchIndex],
-        ...satch,
-      };
+        copyData[index].satchList[satchIndex] = {
+          ...copyData[index].satchList[satchIndex],
+          ...satch,
+        };
 
-      return copyData;
-    });
-  };
+        return copyData;
+      });
+    },
+    [goalId],
+  );
 
-  const deleteSatch = (satchId: Satch['id']) => {
-    setGoalList((prev) => {
-      const copyData = JSON.parse(JSON.stringify(prev)) as Goal[];
+  const deleteSatch = useCallback(
+    (satchId: Satch['id']) => {
+      setGoalList((prev) => {
+        const copyData = JSON.parse(JSON.stringify(prev)) as Goal[];
+        const index = copyData.findIndex((goal) => goal.id === goalId);
+        const satchIndex = copyData[index].satchList.findIndex((data) => data.id === satchId);
 
-      const index = copyData.findIndex((goal) => goal.id === goalId);
+        copyData[index].satchList.splice(satchIndex, 1);
 
-      const satchIndex = copyData[index].satchList.findIndex((data) => data.id === satchId);
-
-      copyData[index].satchList.splice(satchIndex, 1);
-
-      return copyData;
-    });
-  };
+        return copyData;
+      });
+    },
+    [goalId],
+  );
 
   return { createSatch, updateSatch, deleteSatch };
 };
