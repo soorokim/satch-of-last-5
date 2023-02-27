40c6143d45c848fe00126e24bee255fd0796a683
hheeseung <ssigu69@gmail.com>
2023-02-27 21:49:30 +0900

    feat: 구현 진행중 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,13 @@ import HistoryPage from './pages/Initial/HistoryPage';
 import SetSatchItem from './pages/SetSatchItem/SetSatchItem';
 import SetGoals from './pages/SetGoals/SetGoals';
 import CalendarPage from './pages/CalendarPage';
+import Developing from './pages/Developing/Developing';
 
 function App() {
   return (
     <Layout>
       <Routes>
+        <Route path="/developing" element={<Developing />} />
         <Route path="/setsatchitem" element={<SetSatchItem />} />
         <Route path="/setgoals" element={<SetGoals />} />
         <Route path="/" element={<Main />} />
--- src/pages/Main/SatchItemDetail.tsx
+++ src/pages/Main/SatchItemDetail.tsx
@@ -91,7 +91,7 @@ const SatchItemDetail = ({ selectedItem, currentGoal, setOpen }: SatchProps) => 
         <Content>{date}</Content>
       </StringWrapper>
       <ButtonWrapper>
-        <Link to={`/setsatchitem?id=${selectedItem.id}`}>
+        <Link to="/developing">
           <Button color="#79BCF6" bgColor="#FFFFFF">
             수정
           </Button>
