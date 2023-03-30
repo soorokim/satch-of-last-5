11a5100886a58efc1504ce9beed07ba51794f4b1
soorokim <rlatnfh99@gmail.com>
2023-03-30 19:25:08 +0900

    update: 시작하기 버튼 카카오로 시작하기 버튼으로 변경

--- src/components/Initial.tsx
+++ src/components/Initial.tsx
@@ -1,6 +1,6 @@
-import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import InitialTem from '../assets/initialicon.svg';
+import KakaoLoginButton from './KakaoLoginButton';
 
 const Wrap = styled.div`
   box-sizing: border-box;
@@ -17,24 +17,6 @@ const InitialBox = styled.div`
   top: -10%;
 `;
 
-const StartBtn = styled.button`
-  width: 100%;
-  max-width: 375px;
-  height: 52px;
-  border-radius: 100px;
-  color: #ffffff;
-  background-color: #79bcf6;
-  outline: none;
-  border: none;
-  font-family: 'LINE Seed Sans KR';
-  font-style: normal;
-  font-weight: 700;
-  font-size: 16px;
-  line-height: 150%;
-  :focus {
-    outline: none;
-  }
-`;
 const InitialTitleBox = styled.div`
   width: 200px;
   height: 160px;
@@ -46,7 +28,6 @@ const InitialTitleBox = styled.div`
   font-weight: 400;
   font-size: 30px;
   line-height: 150%;
-  border: 1px soild black;
   letter-spacing: 0.04em;
   color: #000000;
 `;
@@ -56,32 +37,23 @@ const InitialEmo = styled.img`
   max-width: 375px;
 `;
 
-const Initial = () => {
-  const navigate = useNavigate();
-  const StartClickHandler = () => {
-    navigate('/setgoals');
-  };
+const Initial = () => (
+  <Wrap>
+    <InitialBox>
+      <InitialTitleBox>
+        <span>
+          내 <span style={{ fontWeight: '700' }}>삿치</span>의
+        </span>
+        <span>
+          <span style={{ fontWeight: '700' }}>목표</span>를
+        </span>
+        <span>정해주세요!</span>
+      </InitialTitleBox>
+    </InitialBox>
+    <InitialEmo src={InitialTem} />
 
-  return (
-    <Wrap>
-      <InitialBox>
-        <InitialTitleBox>
-          <span>
-            내 <span style={{ fontWeight: '700' }}>삿치</span>의
-          </span>
-          <span>
-            <span style={{ fontWeight: '700' }}>목표</span>를
-          </span>
-          <span>정해주세요!</span>
-        </InitialTitleBox>
-      </InitialBox>
-      <InitialEmo src={InitialTem} />
-
-      <div className="buttonWrap">
-        <StartBtn onClick={StartClickHandler}>시작하기</StartBtn>
-      </div>
-    </Wrap>
-  );
-};
+    <KakaoLoginButton />
+  </Wrap>
+);
 
 export default Initial;
