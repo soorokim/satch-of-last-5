dc34c43566d7348db4913f26efd8c1d2d8b3804a
soorokim <rlatnfh99@gmail.com>
2023-05-03 17:15:13 +0900

    fix: kakao appkey 방어로직추가

--- src/hooks/useKakaoSdk.ts
+++ src/hooks/useKakaoSdk.ts
@@ -1,14 +1,18 @@
 import { useCallback, useEffect, useState } from 'react';
 
 const { Kakao } = window;
-
+const KAKAO_SDK_APP_KEY = process.env.VITE_KAKAO_SDK_APP_KEY;
 const useKakaoSdk = () => {
   const [sdk, setSdk] = useState<Window['Kakao']['Auth']>();
 
   useEffect(() => {
     if (!Kakao.isInitialized()) {
       try {
-        Kakao.init(process.env.VITE_KAKAO_SDK_APP_KEY);
+        if (!KAKAO_SDK_APP_KEY) {
+          throw new Error('카카오 로그인을 사용하려면 카카오 SDK APP KEY가 필요합니다.');
+        }
+
+        Kakao.init(KAKAO_SDK_APP_KEY);
         setSdk(Kakao.Auth);
       } catch {
         alert('카카오 SDK사용이 불가능합니다.');
