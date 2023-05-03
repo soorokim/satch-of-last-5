65cf942f1dd356062ca18097d011de112b24c0ea
soorokim <rlatnfh99@gmail.com>
2023-05-03 18:27:55 +0900

    chore: App.tsx에서 AuthProvider 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,12 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 import Layout from './components/Layout';
 import Main from './pages/Main/Main';
 import InitialPage from './pages/Initial/InitialPage';
-import HistoryPage from './pages/Initial/HistoryPage';
+import HistoryPage from './pages/History/HistoryPage';
 import SetSatchItem from './pages/SetSatchItem/SetSatchItem';
 import SetGoals from './pages/SetGoals/SetGoals';
 import CalendarPage from './pages/CalendarPage';
 import Developing from './pages/Developing/Developing';
 import Oauth from './pages/Oauth/Oauth';
-import AuthProvider from './context/AuthProvider';
 import BottomNavigationBar from './components/BottomNavigationBar';
 import useAuth from './hooks/useAuth';
 
@@ -24,22 +23,20 @@ function App() {
   const { hasAuth } = useAuth();
 
   return (
-    <AuthProvider>
-      <Layout>
-        <Routes>
-          <Route path="/initial" element={<InitialPage />} />
-          <Route path="/developing" element={<Developing />} />
-          <Route path="/setsatchitem" element={<SetSatchItem />} />
-          <Route path="/setgoals" element={<SetGoals />} />
-          <Route path="/" element={<Main />} />
-          <Route path="/oauth" element={<Oauth />} />
-          <Route path="/history" element={<HistoryPage />} />
-          <Route path="/calendar" element={<CalendarPage />} />
-        </Routes>
-        <BottomNavigationBar hasAuth={hasAuth} />
-      </Layout>
+    <Layout>
+      <Routes>
+        <Route path="/initial" element={<InitialPage />} />
+        <Route path="/developing" element={<Developing />} />
+        <Route path="/setsatchitem" element={<SetSatchItem />} />
+        <Route path="/setgoals" element={<SetGoals />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/oauth" element={<Oauth />} />
+        <Route path="/history" element={<HistoryPage />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+      </Routes>
+      <BottomNavigationBar hasAuth={hasAuth} />
       <LocationDisplay />
-    </AuthProvider>
+    </Layout>
   );
 }
 
