1f54f8fc477e6d58688a78512fc9f30dbf5a95ba
soorokim <rlatnfh99@gmail.com>
2023-05-04 15:53:18 +0900

    test: KakaoLoginButton 테스트 코드 추가

--- src/__tests__/components/KakaoLoginButton.test.tsx
+++ src/__tests__/components/KakaoLoginButton.test.tsx
@@ -1,8 +1,26 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import KakaoLoginButton from '../../components/KakaoLoginButton';
 
 test('KakaoLoginButton 컴포넌트가 랜더링된다.', async () => {
   const sdk = { requestLogin: jest.fn() };
 
   render(<KakaoLoginButton sdk={sdk} />);
+
+  const button = screen.getByText('카카오로 시작하기');
+
+  expect(button).toBeInTheDocument();
+});
+
+test('KakaoLoginButton 컴포넌트를 클릭하면 sdk의 requestLogin이 실행된다.', async () => {
+  const user = userEvent.setup();
+  const sdk = { requestLogin: jest.fn() };
+
+  render(<KakaoLoginButton sdk={sdk} />);
+
+  const button = screen.getByText('카카오로 시작하기');
+
+  await user.click(button);
+
+  expect(sdk.requestLogin).toBeCalled();
 });
