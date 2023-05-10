f3dccb4a878833f128d74ee5aa900f9927dc883d
soorokim <44357561+soorokim@users.noreply.github.com>
2023-05-10 16:06:48 +0900

    add: react-query 추가

--- src/__tests__/utils/renderWithGlobalState.tsx
+++ src/__tests__/utils/renderWithGlobalState.tsx
@@ -3,10 +3,13 @@ import { JSXElementConstructor, ReactElement } from 'react';
 import userEvent from '@testing-library/user-event';
 import { RecoilRoot } from 'recoil';
 import { MemoryRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import AuthProvider from '../../context/AuthProvider';
 
 // TODO 자동완성 가능하도록 변경
 type RoutePath = string;
+
+const queryClient = new QueryClient();
 const renderWithGlobalState = (
   ui: ReactElement<any, string | JSXElementConstructor<any>>,
   { route = '/', initialState }: { route?: RoutePath; initialState?: ({ set }: any) => void },
@@ -17,9 +20,11 @@ const renderWithGlobalState = (
     ...render(ui, {
       wrapper: ({ children }) => (
         <MemoryRouter initialEntries={[route]}>
-          <RecoilRoot initializeState={initialState}>
-            <AuthProvider>{children}</AuthProvider>
-          </RecoilRoot>
+          <QueryClientProvider client={queryClient}>
+            <RecoilRoot initializeState={initialState}>
+              <AuthProvider>{children}</AuthProvider>
+            </RecoilRoot>
+          </QueryClientProvider>
         </MemoryRouter>
       ),
     }),
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import App from './App';
 import './index.css';
@@ -13,14 +14,18 @@ if (process.env.NODE_ENV === 'test') {
   server.start({ onUnhandledRequest: 'bypass' });
 }
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <RecoilRoot>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </RecoilRoot>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
