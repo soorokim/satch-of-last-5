b227bdbfde3ed57e94e8cd8cef91dc0ba8da0c52
Heeseung Ha <ssigu69@gmail.com>
2023-06-16 22:11:31 +0900

    refactor: 삿치템 등록 코드 수정

--- src/pages/SetSatchItem/SetSatchItem.tsx
+++ src/pages/SetSatchItem/SetSatchItem.tsx
@@ -1,5 +1,42 @@
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { useForm } from 'react-hook-form';
 import SetSatchForm from '../../components/SetSatchForm';
+import { goalListState } from '../../atoms/goalList';
+import useGoal from '../../hooks/useGoal';
 
-const SetSatchItem = () => <SetSatchForm isEdit={false} />;
+interface SatchProps {
+  name: string;
+  price: number;
+  date: Date;
+}
+
+const SetSatchItem = () => {
+  const navigate = useNavigate();
+  const goalList = useRecoilValue(goalListState);
+  const goalId = goalList[goalList.length - 1].id;
+  const { createSatch } = useGoal(goalId);
+  const { register, handleSubmit, formState } = useForm<SatchProps>();
+
+  const onValid = ({ name, price, date }: SatchProps) => {
+    const satchPrice = Number(price);
+
+    createSatch({
+      name,
+      price: satchPrice,
+      date,
+    });
+    navigate('/');
+  };
+
+  return (
+    <SetSatchForm
+      register={register}
+      formState={formState}
+      handleSubmit={handleSubmit}
+      onValid={onValid}
+    />
+  );
+};
 
 export default SetSatchItem;
